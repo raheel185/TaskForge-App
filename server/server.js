a3ca2295fa43e231808b2c0fa405dc56da425f0d
Raheel Fida <nabeelfida185@gmail.com>
2026-03-30 19:40:10 +0500

    Inngest Setup

--- server/server.js
+++ server/server.js
@@ -2,12 +2,15 @@ import express from 'express'
 import "dotenv/config"
 import cors from 'cors'
 import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.use(clerkMiddleware())
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get('/', (req, res)=> res.send('Server is live.'))
 
