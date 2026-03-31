977aa0bd125a6de356673ae88810eb48e73403d5
Raheel Fida <nabeelfida185@gmail.com>
2026-03-31 16:32:53 +0500

    Inngest Connection with Clerk and Prisma

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,7 +1,48 @@
 import { Inngest } from "inngest";
+import prisma from "../prisma.config.js"
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "my-app" });
+export const inngest = new Inngest({ id: "taskforge-app" });
 
 // Create an empty array where we'll export future Inngest functions
 export const functions = [];
+
+// Inngest function to Save user to database
+
+export const syncUserCreation = inngest.createFunction(
+   {
+    id: 'sync-user-with-clerk',
+    triggers: [{ event: 'clerk/user.created' }],
+  },
+  async ({ event }) => {
+    const { data } = event
+
+    await prisma.user.create({
+      data: {
+        id: data.id,
+        email: data.email_addresses[0].email_address,
+        name: `${data.first_name ?? ''} ${data.last_name ?? ''}`.trim(),
+      },
+    })
+  }
+)
+
+// Inngest function to Delete User from database
+
+export const syncUserDeletion = inngest.createFunction(
+   {
+    id: 'delete-user-with-clerk',
+    triggers: [{ event: 'clerk/user.deleted' }],
+  },
+  async ({ event }) => {
+    const { data } = event
+
+    await prisma.user.create({
+      data: {
+        id: data.id,
+        email: data.email_addresses[0].email_address,
+        name: `${data.first_name ?? ''} ${data.last_name ?? ''}`.trim(),
+      },
+    })
+  }
+)
--- server/server.js
+++ server/server.js
@@ -17,3 +17,4 @@ app.get('/', (req, res)=> res.send('Server is live.'))
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
