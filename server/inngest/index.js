cf19943f0e33ca1020cdd31a18b133fe08635255
Raheel Fida <nabeelfida185@gmail.com>
2026-03-31 16:38:24 +0500

    Inngest Update

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -8,7 +8,6 @@ export const inngest = new Inngest({ id: "taskforge-app" });
 export const functions = [];
 
 // Inngest function to Save user to database
-
 export const syncUserCreation = inngest.createFunction(
    {
     id: 'sync-user-with-clerk',
@@ -22,13 +21,13 @@ export const syncUserCreation = inngest.createFunction(
         id: data.id,
         email: data.email_addresses[0].email_address,
         name: `${data.first_name ?? ''} ${data.last_name ?? ''}`.trim(),
+        image: data?.image_url,
       },
     })
   }
 )
 
 // Inngest function to Delete User from database
-
 export const syncUserDeletion = inngest.createFunction(
    {
     id: 'delete-user-with-clerk',
@@ -37,11 +36,31 @@ export const syncUserDeletion = inngest.createFunction(
   async ({ event }) => {
     const { data } = event
 
-    await prisma.user.create({
+    await prisma.user.delete({
+      where: {
+        id: data.id
+      },
+    })
+  }
+)
+
+// Inngest function to Update user data in database
+export const syncUserUpdate = inngest.createFunction(
+   {
+    id: 'update-user-with-clerk',
+    triggers: [{ event: 'clerk/user.updated' }],
+  },
+  async ({ event }) => {
+    const { data } = event
+
+    await prisma.user.update({
+      where: {
+        id: data.id
+      },
       data: {
-        id: data.id,
         email: data.email_addresses[0].email_address,
         name: `${data.first_name ?? ''} ${data.last_name ?? ''}`.trim(),
+        image: data?.image_url,
       },
     })
   }
